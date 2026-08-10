2246ea50be6896d4951b126f50aa2b140ab57c34
DiegoRodolico <rodolicodiego@gmail.com>
2026-08-10 11:59:09 -0300

    agregado base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://DiegoRodolico.github.io/portfolio/'
+  site: 'https://DiegoRodolico.github.io/portfolio/',
+  base: '/portfolio/',
 });
